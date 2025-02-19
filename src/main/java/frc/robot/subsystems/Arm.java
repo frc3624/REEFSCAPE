3e56b175e06e3613d2ab7c168c394b175eeda4ed
a-warraich <107781060+a-warraich@users.noreply.github.com>
2025-02-19 15:21:34 -0500

    Update Arm.java

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -18,11 +18,15 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkAbsoluteEncoder;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.ClosedLoopSlot;
 import static frc.robot.Constants.Motors.*;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.spark.SparkClosedLoopController;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Arm extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
@@ -30,13 +34,12 @@ public class Arm extends SubsystemBase {
   private final SparkMax follower = new SparkMax(rightMotor, MotorType.kBrushless);
 
   private final SparkAbsoluteEncoder encoder = leader.getAbsoluteEncoder();
-  private SparkClosedLoopController armPIDController;
   
-  //To Be Tuned
-  private final SlewRateLimiter limiter = new SlewRateLimiter(25);
-
+  private double setpoint = 0;
+  
+  private SparkClosedLoopController armPIDController = leader.getClosedLoopController();
+  
   public Arm() {
-    armPIDController = leader.getClosedLoopController();
     configureMotors();
   }
 
@@ -44,25 +47,29 @@ public class Arm extends SubsystemBase {
     SparkMaxConfig leaderConfig = new SparkMaxConfig();
     SparkMaxConfig followerConfig = new SparkMaxConfig();
 
-    leaderConfig.encoder.positionConversionFactor(360).velocityConversionFactor(360);
-    leaderConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder);
-    leaderConfig.closedLoop.pid(0, 0, 0);
+    leaderConfig.encoder.positionConversionFactor(1).velocityConversionFactor(1);
+    leaderConfig.closedLoop.pid(0.03, 0, 0);
+
     leaderConfig.closedLoopRampRate(2);
     followerConfig.closedLoopRampRate(2);
+
+    followerConfig.follow(leftMotor);
 
     follower.configure(followerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     leader.configure(leaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   public double getPosition(){
-    double pos = encoder.getPosition();
-    pos = Math.round(pos * 1000.0) / 1000.0;
-    return pos;
+    double position = encoder.getPosition();
+    
+    return position;
   }
 
   public void setPosition(double pos){
-    armPIDController = leader.getClosedLoopController();
-    armPIDController.setReference(pos, SparkMax.ControlType.kPosition);
+    this.setpoint = pos;
+
+
+    armPIDController.setReference(setpoint, ControlType.kPosition, ClosedLoopSlot.kSlot0);
   }
 
   public void setSpeed(double speed){
@@ -70,9 +77,20 @@ public class Arm extends SubsystemBase {
     leader.set(speed);
   }
 
+  public boolean reached(){
+    if((setpoint - .001 <= encoder.getPosition()) && (encoder.getPosition() <= setpoint + .001)){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Position", encoder.getPosition());
+    SmartDashboard.putNumber("SetPosition", setpoint);
   }
 
   @Override
